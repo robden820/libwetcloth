ca966662f45dcdaf364693ac049bb5b4e732077e
Rob Dennison <rdennison11@gmail.com>
2023-04-11 15:42:00 +0100

    Batch files to run sim w & w/o houdini output

---
+++ RunSim.bat
@@ -0,0 +1,3 @@
+cd C:\Users\Robert Dennison\Documents\GitHub\libwetcloth\build\libWetCloth\App\Release
+WetClothApp -s "..\..\..\..\assets\fluid_tests\default_fluid_small.xml" -p 0
+PAUSE
---
+++ RunSim_Houdini.bat
@@ -0,0 +1,3 @@
+cd houdini
+..\build\libWetCloth\App\Release\WetClothApp -s ../assets/fluid_tests/default_fluid_small.xml -o 20 -p 0 -d 1
+PAUSE
